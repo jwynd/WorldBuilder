b7483049cb0f61ad6bf6c80f8d491dab1600b259
jwynd <18224844+jwynd@users.noreply.github.com>
2019-11-02 19:07:24 -0700

    bringing riverAgent in line with semistandard style

--- WorldBuilder/js/riverAgent.js
+++ WorldBuilder/js/riverAgent.js
@@ -1,60 +1,66 @@
 // River agent file
 // Contains class definition and generate function
+/* global
+  getRandomPointOfType
+  random
+*/
+/* jshint esversion: 6 */
+class RiverAgent {
+  constructor (maxRivers = 1) {
+    this.maxRivers = maxRivers;
+  }
 
-class RiverAgent{
-    constructor(maxRivers = 1){
-        this.maxRivers = maxRivers;
+  // will generate maxRivers
+  generate (map) {
+    const altered = [];
+    for (let i = 0; i < this.maxRivers; ++i) {
+      Array.prototype.push.apply(altered, this.generateRiver(map)); // only way I found to concatinate arrays
     }
+    return altered;
+  }
 
-    // will generate maxRivers
-    generate(map){
-        let altered = [];
-        for(let i = 0; i < this.maxRivers; ++i){
-            Array.prototype.push.apply(altered, generateRiver(map)); // only way I found to concatinate arrays
+  // will generate 1 river
+  generateRiver (map) {
+    this.defineBeach(map);
+    const b = getRandomPointOfType('beach');
+    const m = getRandomPointOfType('mountain');
+    let p = b;
+    const d = b.dist(m); // want to reduce the distance between b and m
+    const altered = [];
+    while (p.getBiome() !== 'mountain') {
+      // move towards a mountain
+      p.setBiome('river');
+      altered.push(p);
+      const nRaw = map.getNeighbors(p);
+      const n = [];
+      for (const point of nRaw) {
+        const biome = point.getBiome();
+        if (biome === 'ocean' || biome === 'river' || biome === 'beach') {
+          continue;
         }
-        return altered;
-    }
-    
-    // will generate 1 river
-    generateRiver(map){
-        defineBeach(map);
-        let b = getRandomPointOfType("beach");
-        let m = getRandomPointOfType("mountain");
-        let p = b;
-        let d = b.dist(m); // want to reduce the distance between b and m
-        let altered = []
-        while(p.getBiome() !== "mountain"){
-            // move towards a mountain
-            p.setBiome("river");
-            altered.push(p);
-            let nRaw = map.getNeighbors(p);
-            let n = [];
-            for(point of nRaw){
-                let biome = point.getBiome();
-                if(biome === "ocean" || biome === "river" || biome === "beach"){
-                    continue;
-                }
-                if(point.dist(m) > d){
-                    continue;
-                }
-                n.push(point);
-            }
-            if(n.length === 0){
-                console.error("failed river");
-                for(point of altered){
-                    point.setBiome("xriver");
-                }
-            }
-            p = random(n);
+        if (point.dist(m) > d) {
+          continue;
         }
-        return altered;
+        n.push(point);
+      }
+      if (n.length === 0) {
+        console.error('failed river');
+        for (const point of altered) {
+          point.setBiome('xriver');
+        }
+      }
+      p = random(n);
     }
+    return altered;
+  }
 
-    defineBeach(map){
-        let o = map.getPointsOfType("ocean");
-        for(p of o){
-            let n = map.getNeighborsOfType(p, "coast");
-            n.forEach(function(point, index){point.setBiome("beach")});
-        }
+  defineBeach (map) {
+    const o = map.getPointsOfType('ocean');
+    for (const p of o) {
+      const ns = map.getNeighborsOfType(p, 'coast');
+      for (const n of ns) {
+        n.setBiome('beach');
+      }
     }
+  }
 }
