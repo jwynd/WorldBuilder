0c7d381a1351620751112870cfffb9ef074fa842
jwynd <18224844+jwynd@users.noreply.github.com>
2019-11-04 17:10:35 -0800

    added function dir to point class

--- WorldBuilder/js/generateMap.js
+++ WorldBuilder/js/generateMap.js
@@ -48,6 +48,21 @@ class Point {
     return sqrt(((x1 - x2) * (x1 - x2)) + ((y1 - y2) * (y1 - y2)));
   }
 
+  // return the direction from this to p.
+  dir (p) {
+    const x1 = this.getX();
+    const y1 = this.getY();
+    const x2 = p.getX();
+    const y2 = p.getY();
+    let phrase1 = '';
+    let phrase2 = '';
+    if (y1 < y2) phrase1 = 'south';
+    if (y1 > y2) phrase1 = 'north';
+    if (x1 < x2) phrase2 = 'east';
+    if (x1 > x2) phrase2 = 'west';
+    return phrase1.concat(phrase2);
+  }
+
   toString () {
     return 'Point: (' + this.x + ', ' + this.y + ')\nElevation: ' + this.elevation + '\nBiome: ' + this.biome;
   }
