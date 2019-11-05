eea8d1c80a2bbc5c54d44431f8456c3c1c17cf60
jwynd <18224844+jwynd@users.noreply.github.com>
2019-11-04 17:50:53 -0800

    River agent integrated, requires optimization for speed

--- WorldBuilder/js/main.js
+++ WorldBuilder/js/main.js
@@ -12,12 +12,14 @@
   MountainAgent
   map
   lerpColor
+  RiverAgent
 */
 /* jshint esversion: 6 */
 let heightmap;
 let m;
 let c;
 let ma; // mountain agent
+let r; // river agent
 const mWidth = 1280;
 const mHeight = 720;
 const tokens = 300000;
@@ -27,8 +29,8 @@ const worldSeed = 0xa12413adff;
 // /////////////////
 // Mountain Params//
 // /////////////////
-const m1 = 8;
-const m2 = 10000;
+const m1 = 30;
+const m2 = 1000;
 const m3 = 25;
 const m4 = 5;
 const m5 = 10;
@@ -47,7 +49,8 @@ function setup () { // ignore error, called by p5
   const p = m.point(sPointX, sPointY);
   c = new CoastAgent(p, tokens, limit);
   ma = new MountainAgent(m1, m2, m3, m4, m5, m6, m7, m8, m9, m10);
-  const l = [c, ma];
+  r = new RiverAgent(10);
+  const l = [c, ma, r];
   for (let i = 0; i < l.length; i++) {
     l[i].generate(m);
   }
@@ -64,15 +67,15 @@ function makeHeightmap (m) {
       let col = 0;
       if (raw === 'ocean') {
         col = color(0, 0, 255);
-      } else if (raw === 'coast') {
-        console.log('c_elevation ' + m.point(i, j).getElevation());
+      } else if (raw === 'coast' || raw === 'beach') {
         col = color(0, 255, 0);
       } else if (raw === 'mountain') {
         const col1 = color(0, 255, 0);
         const col2 = color(255, 0, 0);
         const elvLerp = map(m.point(i, j).getElevation(), 0, 255, 0, 1, true);
-        console.log('M_elevation ' + m.point(i, j).getElevation());
         col = lerpColor(col1, col2, elvLerp);
+      } else if (raw === 'river') {
+        col = color(0, 0, 0);
       }
       heightmap.set(i, j, col);
     }
