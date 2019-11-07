b05b555b35e78c0893ff887c0771da1f42408722
jwynd <18224844+jwynd@users.noreply.github.com>
2019-11-06 18:06:56 -0800

    removed point class definition from generateMap.js

--- WorldBuilder/app/src/js/generateMap.js
+++ WorldBuilder/app/src/js/generateMap.js
@@ -1,72 +1,6 @@
 
-/*
- * This file will create define a point
- * create an array of points which will define the map
- * call each individual agent from different files to actually
- * alter the point values
- */
 /* jshint esversion: 6 */
 /* global sqrt random */
-// create a point of elevation e, biome b, created by agent a
-class Point {
-  constructor (e, b, xpos, ypos) {
-    this.elevation = e;
-    this.biome = b;
-    this.x = xpos;
-    this.y = ypos;
-  }
-
-  getX () {
-    return this.x;
-  }
-
-  getY () {
-    return this.y;
-  }
-
-  getElevation () {
-    return this.elevation;
-  }
-
-  getBiome () {
-    return this.biome;
-  }
-
-  setElevation (e) {
-    this.elevation = e;
-  }
-
-  setBiome (b) {
-    this.biome = b;
-  }
-
-  dist (p) {
-    const x1 = this.getX();
-    const y1 = this.getY();
-    const x2 = p.getX();
-    const y2 = p.getY();
-    return sqrt(((x1 - x2) * (x1 - x2)) + ((y1 - y2) * (y1 - y2)));
-  }
-
-  // return the direction from this to p.
-  dir (p) {
-    const x1 = this.getX();
-    const y1 = this.getY();
-    const x2 = p.getX();
-    const y2 = p.getY();
-    let phrase1 = '';
-    let phrase2 = '';
-    if (y1 < y2) phrase1 = 'south';
-    if (y1 > y2) phrase1 = 'north';
-    if (x1 < x2) phrase2 = 'east';
-    if (x1 > x2) phrase2 = 'west';
-    return phrase1.concat(phrase2);
-  }
-
-  toString () {
-    return 'Point: (' + this.x + ', ' + this.y + ')\nElevation: ' + this.elevation + '\nBiome: ' + this.biome;
-  }
-}
 
 class Map {
   constructor (x, y) {
@@ -171,3 +105,4 @@ class Map {
     return random(dir);
   }
 }
+export default 
