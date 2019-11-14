862ca7dd3ddb12fe4810290a586814eed1c38214
jwynd <18224844+jwynd@users.noreply.github.com>
2019-11-13 23:03:40 -0800

    rivers follow contours of the land

--- WorldBuilder/app/src/js/riverAgent.js
+++ WorldBuilder/app/src/js/riverAgent.js
@@ -4,6 +4,7 @@
 /* jshint esversion: 6 */
 import Point from './point.js';
 import Map from './map.js';
+// let rcount = 0;
 class RiverAgent {
   constructor (maxRivers = 1, wanderSteps = 1000, wanderCut = 50) {
     this.maxRivers = maxRivers;
@@ -13,30 +14,21 @@ class RiverAgent {
 
   // will generate maxRivers
   generate (map) {
-    this.defineBeach(map);
     const altered = [];
     for (let i = 0; i < this.maxRivers; ++i) {
       console.log('river ' + (i + 1));
+      // rcount++;
       Array.prototype.push.apply(altered, this.generateRiver(map)); // only way I found to concatinate arrays
     }
     return altered;
-  }
-
-  defineBeach (map) {
-    const c = map.getPointsOfType('coast');
-    for (const p of c) {
-      if (map.getNeighborsOfType(p, 'ocean').length > 0) {
-        p.setBiome('beach');
-      }
-    }
   }
 
   // will generate 1 river
   generateRiver (map) {
     const b = map.getRandomPointOfType('beach');
     const m = map.getRandomPointOfType('mountain');
+    console.log(b, m);
     let p = b;
-    let wander = null;
     let d = b.dist(m); // want to reduce the distance between b and m
     const altered = [];
     let count = 0;
@@ -47,23 +39,15 @@ class RiverAgent {
       altered.push(p);
       const nRaw = map.getNeighbors(p);
 
-      if (wander === null) {
-        nRaw.sort(function (p1, p2) { return p1.dist(m) - p2.dist(m); });
-        for (const point of nRaw) {
-          wander = this.generateWanderPoint(map, p, p.dir(point));
-          if (wander !== null) break;
-        }
-      }
-      // console.log('Wander Point:\n' + wander.toString());
-      d = p.dist(wander);
+      d = p.dist(m);
       const n = [];
 
       for (const point of nRaw) {
         const biome = point.getBiome();
-        if (biome === 'ocean') {
+        if (biome === 'ocean' || biome === 'river') {
           continue;
         }
-        if (point.dist(wander) <= d) {
+        if (point.dist(m) <= d) {
           n.push(point);
         }
       }
@@ -71,75 +55,75 @@ class RiverAgent {
         console.error('failed river');
         return altered;
       }
-      p = pickN(n, wander);
-      if (p.dist(wander) === 0 || count > this.wanderCut) wander = null;
+      p = pickN(n, m);
+      // console.log(p, count);
     }
     return altered;
   }
 
-  // pick a random point to wander to.
-  generateWanderPoint (map, p, dir) {
-    const points = [];
-    let x;
-    let y;
-    switch (dir) {
-      case 'north':
-        x = [floor(-this.wanderSteps / 2), floor(this.wanderSteps / 2)];
-        y = [-this.wanderSteps, 0];
-        break;
-      case 'northeast':
-        x = [0, this.wanderSteps];
-        y = [-this.wanderSteps, 0];
-        break;
-      case 'east':
-        x = [0, this.wanderSteps];
-        y = [floor(-this.wanderSteps / 2), floor(this.wanderSteps / 2)];
-        break;
-      case 'southeast':
-        x = [0, this.wanderSteps];
-        y = [0, this.wanderSteps];
-        break;
-      case 'south':
-        x = [floor(-this.wanderSteps / 2), floor(this.wanderSteps / 2)];
-        y = [0, this.wanderSteps];
-        break;
-      case 'southwest':
-        x = [-this.wanderSteps, 0];
-        y = [0, this.wanderSteps];
-        break;
-      case 'west':
-        x = [-this.wanderSteps, 0];
-        y = [floor(-this.wanderSteps / 2), floor(this.wanderSteps / 2)];
-        break;
-      case 'northwest':
-        x = [-this.wanderSteps, 0];
-        y = [-this.wanderSteps, 0];
-        break;
-      default:
-        console.error('generateWanderPoints invalid direction');
-        return null;
-    }
-    if (x[0] < 0) x[0] = 0;
-    if (x[1] > map.width - 1) x[1] = map.width - 1;
-    if (y[0] < 0) y[0] = 0;
-    if (y[1] > map.height - 1) y[1] = map.height - 1;
-    for (let i = x[0]; i < x[1]; ++i) {
-      for (let j = y[0]; j < y[1]; ++j) {
-        points.push(map.point(i, j));
-      }
-    }
-    if (points.length === 0) return null;
-    return random(points);
-  }
+  // // pick a random point to wander to.
+  // generateWanderPoint (map, p, dir) {
+  //   const points = [];
+  //   let x;
+  //   let y;
+  //   switch (dir) {
+  //     case 'north':
+  //       x = [floor(-this.wanderSteps / 2), floor(this.wanderSteps / 2)];
+  //       y = [-this.wanderSteps, 0];
+  //       break;
+  //     case 'northeast':
+  //       x = [0, this.wanderSteps];
+  //       y = [-this.wanderSteps, 0];
+  //       break;
+  //     case 'east':
+  //       x = [0, this.wanderSteps];
+  //       y = [floor(-this.wanderSteps / 2), floor(this.wanderSteps / 2)];
+  //       break;
+  //     case 'southeast':
+  //       x = [0, this.wanderSteps];
+  //       y = [0, this.wanderSteps];
+  //       break;
+  //     case 'south':
+  //       x = [floor(-this.wanderSteps / 2), floor(this.wanderSteps / 2)];
+  //       y = [0, this.wanderSteps];
+  //       break;
+  //     case 'southwest':
+  //       x = [-this.wanderSteps, 0];
+  //       y = [0, this.wanderSteps];
+  //       break;
+  //     case 'west':
+  //       x = [-this.wanderSteps, 0];
+  //       y = [floor(-this.wanderSteps / 2), floor(this.wanderSteps / 2)];
+  //       break;
+  //     case 'northwest':
+  //       x = [-this.wanderSteps, 0];
+  //       y = [-this.wanderSteps, 0];
+  //       break;
+  //     default:
+  //       console.error('generateWanderPoints invalid direction');
+  //       return null;
+  //   }
+  //   if (x[0] < 0) x[0] = 0;
+  //   if (x[1] > map.width - 1) x[1] = map.width - 1;
+  //   if (y[0] < 0) y[0] = 0;
+  //   if (y[1] > map.height - 1) y[1] = map.height - 1;
+  //   for (let i = x[0]; i < x[1]; ++i) {
+  //     for (let j = y[0]; j < y[1]; ++j) {
+  //       points.push(map.point(i, j));
+  //     }
+  //   }
+  //   if (points.length === 0) return null;
+  //   return random(points);
+  // }
 }
 
 function pickN (ns, dest) {
-  let min = ns[0];
+  let result = ns[0];
   for (const n of ns) {
-    if (dest.dist(n) < dest.dist(min)) {
-      min = n;
+    if (n.getElevation() < result.getElevation()) {
+      result = n;
     }
   }
-  return min;
+  return result;
 }
 export default RiverAgent;
