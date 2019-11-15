92f61c9da5ea7148ab63d42151770e8da3a381e3
YouyouZzz <yzhao118@ucsc.edu>
2019-11-14 23:18:58 -0800

    unload the sprite sheet and download function add in App.js

--- WorldBuilder/app/src/App.js
+++ WorldBuilder/app/src/App.js
@@ -8,6 +8,17 @@ import P5Wrapper from 'react-p5-wrapper';
 import sketch from './js/main.js';
 
 class App extends React.Component {
+  download=()=>{
+    debugger
+    let data =document.getElementById("downloadpic").children[0].children[0].toDataURL().replace("image/png", "image/octet-stream;")
+    let filename="download.png"
+    let save_link = document.createElementNS('http://www.w3.org/1999/xhtml', 'a')
+    save_link.href = data
+    save_link.download = filename
+    let event = document.createEvent('MouseEvents')
+    event.initMouseEvent('click', true, false, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null)
+    save_link.dispatchEvent(event)
+  }
   render() {
       /*
       const logIn = isUserAuthorized ? (
@@ -23,10 +34,12 @@ class App extends React.Component {
                   <div className="fill"></div>
                   <h1 className="App-title">WorldBuilder</h1>
                   <img src={logo} className="App-logo" alt="logo" height="100"/>
-                  <img src={download} className="App-download" alt="download" height="50"/>
+                  <img src={download} className="App-download" alt="download" onClick={this.download.bind(this)} height="50"/>
                   <img src={menu} className="App-menu" alt="menu" height="50"/>
                   <Login />
-                  <P5Wrapper sketch={sketch} />
+                  <div id="downloadpic">
+                    <P5Wrapper sketch={sketch} />
+                  </div>
               </header>
           </div>
       )
